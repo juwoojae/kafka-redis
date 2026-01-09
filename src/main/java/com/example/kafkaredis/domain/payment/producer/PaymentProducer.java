ac0e1cfa9d00b01d8a23791dd4cbdc11bdc18cc9
Juwoojae <juwqq1234@naver.com>
2026-01-09 12:15:29 +0900

    refactor: package 구조 변경 - PaymentProducer 패키지 구조 변경

--- src/main/java/com/example/kafkaredis/domain/payment/producer/PaymentProducer.java
+++ src/main/java/com/example/kafkaredis/domain/payment/producer/PaymentProducer.java
@@ -16,7 +16,7 @@ public class PaymentProducer {
 	// 카프카에 메시지를 보내주는 역할 수행
 	// 스프링 부트와 카프카가 소통할때 필요하느 kafkaTemplate 선언
 	private final KafkaTemplate<String, PaymentCompletedEvent> paymentCompletedEventProducerFactory;
-
+	//
 	public void send(PaymentCompletedEvent event){
 
 		paymentCompletedEventProducerFactory.send(TOPIC_PAYMENT_COMPLETED, event);
